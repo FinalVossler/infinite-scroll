c098474f9107965bd3679a87f2ac0ad507b4299c
Hamza <hamza.khalifa.clashingames@gmail.com>
2023-11-15 13:02:30 +0100

    constant for the number of shown items in the screen

--- src/components/infiniteScroll/tests/InfiniteScroll.cy.tsx
+++ src/components/infiniteScroll/tests/InfiniteScroll.cy.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import TestComponent from "./TestComponent";
 
+const NUMBER_OF_SHOW_ITEMS = 11;
+
 describe("<InfiniteScroll />", () => {
   beforeEach(() => {
     cy.viewport(1920, 1080);
@@ -28,14 +30,14 @@ describe("<InfiniteScroll />", () => {
 
     cy.wait(2000);
     cy.get('[data-cy="infiniteScrollContainer"]').should("be.visible");
-    cy.get('[data-cy="person"]').should("have.length", 11);
+    cy.get('[data-cy="person"]').should("have.length", NUMBER_OF_SHOW_ITEMS);
     cy.get('[data-cy="infiniteScrollContainer"]').scrollTo(0, 1000);
     cy.get('[data-cy="infiniteScrollLoading"]').should("be.visible");
     cy.get('[data-cy="infiniteScrollLoading"]').should("not.exist", {
       timeout: 10000,
     });
-    cy.get('[data-cy="person"]').should("have.length", 11);
+    cy.get('[data-cy="person"]').should("have.length", NUMBER_OF_SHOW_ITEMS);
     cy.get('[data-cy="infiniteScrollContainer"]').scrollTo(0, 1000);
-    cy.get('[data-cy="person"]').should("have.length", 11);
+    cy.get('[data-cy="person"]').should("have.length", NUMBER_OF_SHOW_ITEMS);
   });
 });
